e0d75b487280e2ce2a29c3a3b0ba4d87cb262138
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-07-29 20:57:44 +0800

    Created default chat rooms ui with dummy data

--- app/(chat)/index.tsx
+++ app/(chat)/index.tsx
@@ -25,10 +25,22 @@ export default function Index() {
                         params: { chat: item.id }
                     }}
                 >
-                    <Text>{item.title}</Text>
-                    <View>
+                    <View
+                        style={{
+                            gap: 6,
+                            padding: 16,
+                            width: "100%",
+                            borderRadius: 16,
+                            alignItems: "center",
+                            flexDirection: "row",
+                            backgroundColor: "#262626",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                        <ItemTitleAndDescription title={item.title} description={item.description} />
                         <Ionicons name={"chevron-forward-outline"} size={16} color={"gray"} />
                     </View>
+
                 </Link>
             )
         }}
@@ -43,3 +55,23 @@ export default function Index() {
 }
 
 
+function ItemTitle( {title} : {title: string} ) {
+    return (
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 4}}>
+            <Text style={{ fontSize: 17 }}>
+                {title}
+            </Text>
+        </View>
+    )
+}
+
+function ItemTitleAndDescription( {title, description } : {title: string; description: string; } ) {
+    return (
+        <View style={{ gap: 4 }}>
+            <ItemTitle title={title} />
+            <Text style={{ fontSize: 13, color: "gray"}}>
+                {description.length > 50 ? `${description.substring(0, 50)}...` : description}
+            </Text>
+        </View>
+    )
+}
