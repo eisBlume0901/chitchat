7e1ee8bb73e1f4c311654785c341c2b4bdc50534
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-07-29 09:55:01 +0800

    Added button component for sign in option buttons

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,15 +1,18 @@
 import { View } from "react-native";
 import { Text } from "@/components/Text";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "@/components/Button";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
   return (
-      <SafeAreaView
-        style={{
-            flex: 1,
-        }}
-      >
-          <View>
+      <SafeAreaView style={{flex: 1}}>
+          <View
+            style={{
+                gap: 20,
+                alignItems: "center",
+            }}
+          >
               <Text style={{
                   fontSize: 32,
                   fontWeight: "bold"}}
@@ -17,8 +20,14 @@ export default function Index() {
               <Text>
                   A multi-user chat application
               </Text>
+              <Button style={{ flexDirection: "row", gap: 10, alignItems: "center", justifyContent: "center"}}>
+                  <Ionicons name={"logo-google"} size={16} color="black" />
+                  <Text style={{ color: "black", fontWeight: "500" }}>Continue with Google</Text>
+              </Button>
+              <Button>
+                  Sign in with passkey
+              </Button>
           </View>
       </SafeAreaView>
-
-  );
+    )
 }
