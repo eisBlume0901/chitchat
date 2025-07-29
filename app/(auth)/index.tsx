18228c005a85618c8dcbcc76d90ae7eadaedf591
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-07-29 10:49:05 +0800

    Created sign in ui

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -8,30 +8,47 @@ import { Image } from "expo-image";
 export default function Index() {
   return (
       <SafeAreaView style={{flex: 1}}>
-          <View
-            style={{
-                gap: 20,
-                alignItems: "center",
-            }}
-          >
-              <Image
-                  source={require("@/assets/images/logo.png")}
-                  style={{ width: 100, height: 100 }}
-              />
-              <Text style={{
-                  fontSize: 32,
-                  fontWeight: "bold"}}
-              >ChitChat</Text>
-              <Text>
-                  A multi-user chat application
-              </Text>
-              <Button style={{ flexDirection: "row", gap: 10, alignItems: "center", justifyContent: "center"}}>
+
+          <View style={{ flex: 0.1 }}></View>
+          <View style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 16
+          }}>
+              <View style={{ gap: 20, alignItems: "center" }}>
+                  <Image
+                      source={require("@/assets/images/logo.png")}
+                      style={{ width: 100, height: 100 }}
+                  />
+                  <Text style={{ fontSize: 32, fontWeight: "bold"}}>ChitChat</Text>
+                  <Text>A Multi-User Chat Application</Text>
+              </View>
+
+              <View style={{ flex: 1 }}></View>
+
+              <Button style={{
+                  marginBottom: 20
+              }}>
+                  Sign in with Passkey
+              </Button>
+              <Button
+                  style={{
+                      flexDirection: "row",
+                      gap: 10,
+                      alignItems: "center",
+                      justifyContent: "center"
+                  }}>
                   <Ionicons name={"logo-google"} size={16} color="black" />
-                  <Text style={{ color: "black", fontWeight: "500" }}>Continue with Google</Text>
+                  <Text
+                      style={{
+                          color: "black",
+                          fontWeight: "500"
+                      }}>
+                      Continue with Google
+                  </Text>
               </Button>
-              <Button>
-                  Sign in with passkey
-              </Button>
+
           </View>
       </SafeAreaView>
     )
