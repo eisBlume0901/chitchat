1eaebbaeaf896749289f14b02c7f3e34fd8ecc27
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-07-29 11:49:12 +0800

    Created sign in with passkey and Google auth

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -4,9 +4,66 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "@/components/Button";
 import { Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
+import * as WebBrowser from "expo-web-browser";
+import * as AuthSession from "expo-auth-session";
+import {isClerkAPIResponseError, useSignIn, useSSO} from "@clerk/clerk-expo";
+import {useState} from "react";
+import {ClerkAPIError} from "@clerk/types";
+
+// Since Google is a third-party app, we need to access and revoke the WebBrowser
+// https://clerk.com/docs/custom-flows/enterprise-connections
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Index() {
-  return (
+    const {startSSOFlow } = useSSO();
+    const [errors, setErrors] = useState<ClerkAPIError[]>([]);
+    const { signIn, setActive } = useSignIn();
+
+    const handleSignInWithGoogle = async () => {
+
+        try {
+            const { createdSessionId, setActive } = await startSSOFlow({
+                strategy: "oauth_google",
+                redirectUrl: AuthSession.makeRedirectUri()
+            });
+
+            if (createdSessionId) {
+                setActive!({ session: createdSessionId})
+            } else {
+                // There is no session
+            }
+
+        } catch (err) {
+            if(isClerkAPIResponseError(err)) {
+                setErrors(err.errors)
+            } else {
+                console.error("AUTH (index.tsx, handleSignInWithGoogle): ", err);
+            }
+        }
+    }
+
+    const handleSignInWithPasskey = async() => {
+
+        try {
+            const signInAttempt = await signIn?.authenticateWithPasskey({
+                flow: "discoverable"
+            })
+
+            if (signInAttempt?.status === "complete") {
+                await setActive!({ session: signInAttempt.createdSessionId })
+            } else {
+                // There is no session
+            }
+        } catch (err) {
+            if(isClerkAPIResponseError(err)) {
+                setErrors(err.errors)
+            } else {
+                console.error("AUTH (index.tsx, handleSignInWithPasskey): ", err);
+            }
+        }
+    }
+
+    return (
       <SafeAreaView style={{flex: 1}}>
 
           <View style={{ flex: 0.1 }}></View>
@@ -23,13 +80,19 @@ export default function Index() {
                   />
                   <Text style={{ fontSize: 32, fontWeight: "bold"}}>ChitChat</Text>
                   <Text>A Multi-User Chat Application</Text>
+
+                  {errors.map((error) => (
+                      <Text key={error.code}>{error.code}</Text>
+                  ))}
               </View>
 
               <View style={{ flex: 1 }}></View>
 
               <Button style={{
                   marginBottom: 20
-              }}>
+              }}
+                onPress={handleSignInWithPasskey}
+              >
                   Sign in with Passkey
               </Button>
               <Button
@@ -38,7 +101,9 @@ export default function Index() {
                       gap: 10,
                       alignItems: "center",
                       justifyContent: "center"
-                  }}>
+                  }}
+                  onPress={handleSignInWithGoogle}
+              >
                   <Ionicons name={"logo-google"} size={16} color="black" />
                   <Text
                       style={{
